ba17df941991bfca46aa81949973e59f4b9b93e3
tliddle1 <thomasliddle34@gmail.com>
2019-07-18 08:57:36 -0600

    Update logic.py

--- logic.py
+++ logic.py
@@ -17,6 +17,15 @@ def add_two(mat):
     mat[a][b] = 2
     return mat
 
+def add_four(mat):
+    a = random.randint(0, len(mat)-1)
+    b = random.randint(0, len(mat)-1)
+    while(mat[a][b] != 0):
+        a = random.randint(0, len(mat)-1)
+        b = random.randint(0, len(mat)-1)
+    mat[a][b] = 4
+    return mat
+
 def score(mat):
     """returns the current score of the game given the current matrix"""
     score = 0
